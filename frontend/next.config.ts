3578fa749be18c09d1b9de626c7f5c16d52c5418
omkarg01 <omkargujja01@gmail.com>
2025-11-14 22:07:45 +0530

    nex config set

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  env: {
+    API_BASE_URL: process.env.NEXT_PUBLIC_APP_ENV === "development" ? "http://localhost:3000" : 'https://lovable-clone-bu9m.onrender.com'
+  },
   images: {
     domains: ['images.unsplash.com', 'randomuser.me', 'picsum.photos', "i.pravatar.cc"],
   },
