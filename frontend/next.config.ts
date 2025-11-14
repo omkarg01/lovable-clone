653e0c4bccd100d3a7c505b7bde38088c3b45c58
omkarg01 <omkargujja01@gmail.com>
2025-11-14 21:59:56 +0530

    PROD URL

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  env: {
-    API_BASE_URL: "http://localhost:3000"
-  },
   images: {
     domains: ['images.unsplash.com', 'randomuser.me', 'picsum.photos', "i.pravatar.cc"],
   },
@@ -14,7 +11,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: 'http://localhost:3000/api/:path*', // Your Express backend
+        destination: process.env.NEXT_PUBLIC_APP_ENV === "development" ? 'http://localhost:3000/api/:path*' : 'https://lovable-clone-bu9m.onrender.com/api/:path*', // Your Express backend
       },
     ];
   },
